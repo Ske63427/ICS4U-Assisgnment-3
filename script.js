220b8adc8ea489f1111d4f20a7734d08cb1b546a
Aidan Samaroo <skelecrae63427@gmail.com>
2024-10-17 00:57:59 -0400

    started ambiguous case, and am doing terribly :(

--- script.js
+++ script.js
@@ -2,14 +2,70 @@ const heronCalc = document.getElementById("hfSubmit")
 const hfDisplay = document.getElementById("hfAnswerBlock")
 
 function heronsFormula(){
-    let a = document.getElementByID("hfA");
-    let b = document.getElementByID("hfB");
-    let c = document.getElementByID("hfC");
-    let hfAnswer = 0.25 * Math.sqrt((4*a**2*b**2)-(a**2+b**2-c**2)**2)
-    hfDisplay.setAttribute("placeholder", hfAnswer)
+    let a1 = Number(document.getElementById("hfA").value)
+    let b1 = Number(document.getElementById("hfB").value)
+    let c1 = Number(document.getElementById("hfC").value)
+    let hfAnswer = 0.25 * Math.sqrt((4  *a**2 * b**2)-(a**2 + b**2 - c**2)**2)
+    hfDisplay.value = hfAnswer
     console.log(hfAnswer)
 }
 
-heronCalc.addEventListerner("click", () => {
-    heronsFormula()
-})
+heronCalc.addEventListener("click", () => {
+    heronsFormula();
+});
+
+//
+const acDisplay = document.getElementById("acAnswerBlock")
+const acCalc = document.getElementById("acSubmit")
+const possibleTriangles = ["No Triangle", "Right Triangle", "One Triangle", "Two Triangles"]
+
+function ambiguousCase(){
+    let a2 = Number(document.getElementById("acA").value)
+    let b2 = Number(document.getElementById("acB").value)
+    let angleA = Number(document.getElementById("acAA").value)
+    let h = b2*Math.sin(a2)
+    console.log(`${a2}, ${b2}, ${angleA}, ${h}`)
+
+    // let (acDisplay.value) = 
+    //     angleA < 90
+    //         ? (a2 < h)
+    //             ? possibleTriangles[0]
+    //             : (a2 = h)
+    //                 ? possibleTriangles[1]
+    //                 : (a2 > b2)
+    //                     ? possibleTriangles[2]
+    //                     : possibleTriangles[3]
+    //         : (a2 > b2)
+    //             ? possibleTriangles[2]
+    //             : possibleTriangles[0]
+
+
+    if (angleA < 90){
+        if (a < h){
+            let triangle = "No Triangle"
+            console.log(possibleTriangles[0])
+        } else if (a = h){
+            let triangle = "Right Triangle"
+            console.log(possibleTriangles[1])
+        } else if (a > b){
+            let triangle = "One Triangle"
+            console.log(possibleTriangles[2])
+        } else {
+            let triangle = "Two Triangles"
+            console.log(possibleTriangles[3])
+        }
+    } else {
+        if ((a < b) || (a = h)){
+            let triangle = "No Triangles"
+            console.log(possibleTriangles[0])
+        } else if (a > b){
+            let triangle = "One Triangle"
+            console.log(possibleTriangles[2])
+        }
+    }
+    //console.log(triangle)
+}
+
+acCalc.addEventListener("click", () => {
+    ambiguousCase();
+});
