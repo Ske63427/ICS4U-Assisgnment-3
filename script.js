de0a7e9358c6c5b7d20be520a726f6c71a50819e
email <skelecrae63427@gmail.com>
2024-10-25 10:28:59 -0400

    pushing for 2nd period

--- script.js
+++ script.js
@@ -11,12 +11,9 @@ function clearFields(hfFields, acFields){
         document.getElementById("acAA").value = " ";
     }
 }
-
+/*------------------------------------------------------------------------------------------------------------------------------------------*/
 const heronCalc = document.getElementById("hfSubmit")
 const hfDisplay = document.getElementById("hfAnswerBlock")
-// let a1 = Number(document.getElementById("hfA").value)
-// let b1 = Number(document.getElementById("hfB").value)
-// let c1 = Number(document.getElementById("hfC").value)
 
 function heronsFormula(a, b, c){
     let hfAnswer = 0.25 * Math.sqrt((4 * a**2 * b**2)-(a**2 + b**2 - c**2)**2)
@@ -25,52 +22,50 @@ function heronsFormula(a, b, c){
 }
 
 heronCalc.addEventListener("click", () => {
-    clearFields(false, true);
-    heronsFormula(document.getElementById("hfA").value, document.getElementById("hfB").value, document.getElementById("hfC").value);
+    // clearFields(false, true);
+    let a1 = Number(document.getElementById("hfA").value)
+    let b1 = Number(document.getElementById("hfB").value)
+    let c1 = Number(document.getElementById("hfC").value)
+    heronsFormula(a1, b1, c1);
 });
 
 //-----------------------------------------------------------------------------------------------------------------
 const acDisplay = document.getElementById("acAnswerBlock")
 const acCalc = document.getElementById("acSubmit")
 
-function toRadians (angle) { //forgetting that i had to convert to radians has caused me days of pain
+function toRadians(angle) { //forgetting that i had to convert to radians has caused me days of pain
     return angle * (Math.PI/180);
 }
 
-function ambiguousCase(){
-    let a2 = document.getElementById("acA").value
-    let b2 = document.getElementById("acB").value
-    let A = toRadians(document.getElementById("acAngleA").value)
-    let h = Math.round(b2*Math.sin(A))
-
-    console.log(`${h} = ${b2}*Math.sin(${A})`)
-    console.log(`a:${a2}, b:${b2}, A:${A}, h:${h}`)
+function ambiguousCase(a, b, A, h){
+    console.log(`${h} = ${b}*Math.sin(${A})`)
+    console.log(`a:${a}, b:${b}, A:${A}, h:${h}`)
     if (A < 90){
         console.log("A < 90")
-        if ((a2 > h) && (a2 < b2)) {
+        if ((a > h) && (a < b)) {
             let triangle = "Two Triangles"
-            console.log(`Two Triangles. ${h} < ${a2} < ${b2}`)
+            console.log(`Two Triangles. ${h} < ${a} < ${b}`)
             acDisplay.value = triangle
-        } else if (a2 == h){
+        } else if (a == h){
             let triangle = "Right Triangle"
-            console.log(`Right Triangle. ${h} == ${a2}`)
+            console.log(`Right Triangle. ${h} == ${a}`)
             acDisplay.value = triangle
-        } else if (a2 < h){
+        } else if (a < h){
             let triangle = "No Triangle"
-            console.log(`No Triangles. ${h} < ${a2}`)
+            console.log(`No Triangles. ${h} < ${a}`)
             acDisplay.value = triangle
         } else {
             let triangle = "One Triangle"
-            console.log(`One Triangle. ${a2} > ${b2}`)
+            console.log(`One Triangle. ${a} > ${b}`)
             acDisplay.value = triangle
         }
     } else {
         console.log("A !< 90")
-        if ((a2 < b2) || (a2 == h)){
+        if ((a < b) || (a == h)){
             let triangle = "No Triangles"
             console.log("No Triangle")
             acDisplay.value = triangle
-        } else if (a2 > b2){
+        } else if (a > b){
             let triangle = "One Triangle"
             console.log("One Triangle")
             acDisplay.value = triangle
@@ -80,13 +75,20 @@ function ambiguousCase(){
 
 acCalc.addEventListener("click", () => {
     // clearFields(true, false)
-    ambiguousCase();
+    let a2 = document.getElementById("acA").value
+    let b2 = document.getElementById("acB").value
+    let A = toRadians(document.getElementById("acAngleA").value)
+    let h = Math.round(b2*Math.sin(A))
+    ambiguousCase(a2, b2, A, h);
 });
 /*----------------------------------------------------------------------*/
 /*Newtons Method */
 /*----------------------------------------------------------------------*/
 /*Polynomial Formula */
 
-function polynomial(){
 
+function polynomial(){
+    let coefficients = document.getElementById("pfC").value
+    let Exponents = document.getElementById("pfE").value
+    let A = document.getElementById("pfX").value
 }
